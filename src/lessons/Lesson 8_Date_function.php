5f7d46b26f3958befcd95670e75b8c581139209f
QuietHours <xskilldp@gmail.com>
2020-10-13 14:09:18 +0300

    Added Les9 working_with_files.php and Edited les 8_data functions.php

--- src/lessons/Lesson 8_Date_function.php
+++ src/lessons/Lesson 8_Date_function.php
@@ -3,5 +3,21 @@ $title = "Date function";
 include_once "../header.php"; //подключает файл 
 include_once "../footer.php";
 
+    echo time()."<br/>";//возвращает кол-во секунд прошедших с 1 января 1970 года
+    //был основан UNIX
 
+    echo microtime ()."<br/"; //Вывод в мили секундах 
+    //замеры времени
+    $start = microtime(true);
+    echo "Время работы скрипта " . (microtime(true)-$start). "<br/>";
+
+    $time = mktime (12,35,23,12,7,2007); //записывается дата 
+
+    echo date("Y-m-d H:i:s", $time)."<br/>";//Вывод даты, времени
+
+$array = getdate($time);
+print_r($array)."<br/>" //выводи массивом дату
+
+echo "<br/>".checkdate (12,02,2020);//проверка boolean сущетсвует ли такой день
+    
 ?>
