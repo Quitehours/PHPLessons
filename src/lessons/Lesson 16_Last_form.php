d79a09d2cc77d7cd68bac29e811d5643fddccdbe
QuietHours <xskilldp@gmail.com>
2020-10-14 23:58:14 +0300

    php end

---
+++ src/lessons/Lesson 16_Last_form.php
@@ -0,0 +1,72 @@
+<?php
+    session_start();
+    if(isset($_POST["send"])){
+        $from = htmlspecialchars ($_POST["from"]);
+        $to = htmlspecialchars ($_POST["to"]);
+        $subject = htmlspecialchars ($_POST["subject"]);
+        $message = htmlspecialchars ($_POST["message"]);
+        $_SESSION["from"] = $from;
+        $_SESSION["to"] = $to;
+        $_SESSION["subject"] = $subject;
+        $_SESSION["message"] = $message;
+        echo $from . $to . $subject . $message;
+        $error_from = "";
+        $error_to = "";
+        $error_subject = "";
+        $error_message = "";
+        $error = false;
+        if(($from == "") || (!preg_match("/@/", $from))){
+            $error_from = "Enter correct email";
+            $error=true;
+        }
+        if($to =="" || !preg_match("/@/", $from)){
+            $error_to = "Enter corrcet email";
+            $error=true;
+        }
+        if(strlen($subject)==0){
+            $error_subject = "Enter theme";
+            $error=true;
+        }
+        if(strlen($message)==0){
+            $error_message = "Enter message";
+            $error=true;
+        }
+        if(!$error){
+            $subject = "=?utf-8?B?".base64_encode($subject)."?=";
+            $headers = "From: $from \r\nReply-to: $from\r\nContent-type:text/plain; charset=utf-8\r\n";
+            mail($to, $subject, $message, $headers);
+            header("Location:success.php?send=1");
+            exit;
+        }
+        echo $error_from . $error_to . $error_subject . $error_message;
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Feedback</title>
+    </head>
+    <body>
+        <h2>Form Feedback</h2>
+        <form name="feedback" action="" method="POST">
+
+            <label>From who:</label></br>
+            <input type="text" name="from" value="<?=$_SESSION["from"]?>"/>
+            </br><span style="color:red"><?$error_from?></span>
+
+            <label>From to:</label></br>
+            <input type="text" name="to" value="<?=$_SESSION["to"]?>">
+            <span style="color:red"><?$error_to?></span></br>
+
+            <label>Theme:</label></br>
+            <input type="text" name="subject" value="<?=$_SESSION["subject"]?>"/>
+            <span style="color:red"><?$error_subject?></span></br>
+            
+            <label>Message:</label></br>
+            <textarea name="message" cols="30" rows="10"><?=$_SESSION["message"]?></textarea> 
+            <span style="color:red"><?$error_message?></span></br>
+            
+            <input type="submit" name="send" value="Send "/>
+       
+    </body>
+</html>
