d1e74656f7114adeea0621fc6c7e1684b9d5e5dc
QuietHours <xskilldp@gmail.com>
2020-10-15 20:31:29 +0300

    Added new files

--- src/index.php
+++ src/index.php
@@ -1,21 +1,31 @@
-<!-- <!Doctype html>
-<html>
-<head>
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8" /> 
-</head>
-<body> -->
 <?php
 $title = "Home page";
 include_once "header.php"; //подключает файл 
 include_once "footer.php";
+?>
 
-const DIR = 'lessons';
+<div style="width:500px; height:500px; background-color:yellow; float:left">
+<h1 align="center">PHP Lessons</h1>
+<?php
+const DIR = 'PHP_Lessons';
 $scan = scandir(DIR);
     for($i=2;$i<=count($scan);$i++ ){
 ?>
-    <a href="<?= "/lessons/" . $scan[$i] ?>"><?= $scan[$i] ?></a><br/>
+    <a href="<?= "/PHP_Lessons/" . $scan[$i] ?>"><?= $scan[$i] ?></a><br/>
 <?  
 }
 ?>
-<!-- </body>
-</html> -->
+</div>
+
+<div style="width:500px; height:500px; background-color:rgb(102, 102, 153); float:left">
+<h1 align="center">CSS Lessons</h1>
+<?php
+const CSSDIR = 'CSS_Lesson';
+$scanCSS = scandir(CSSDIR);
+    for ($i=2;$i<=count($scanCSS);$i++){
+?>
+    <a href="<?= "/CSS_Lesson/" . $scanCSS[$i] ?>"><?=$scanCSS[$i]?></a><br/>
+<?  
+}
+?>
+</div>
