afcfd4b9bb76a3d36b449e8717fdcd334196bc80
QuietHours <xskilldp@gmail.com>
2020-11-07 23:48:25 +0200

    new commit

--- src/JavaScript_Lesson/app.js
+++ src/JavaScript_Lesson/app.js
@@ -3,6 +3,47 @@
 //window.prompt('Как тебя зовут?') //ввод от пользователя
 //window.confirm('Как тебя зовут?')//подтвердить, либо отменить действие
 
-//document.getElementById('Hello') - елемент по айди поиска ищем
-const heading = document.getElementById('Hello') //document - объект представляющий дом дерево  
-console.log(heading)
+// document.getElementById('Hello') //- елемент по айди поиска ищем
+// const heading = document.getElementById('Hello') //document - объект представляющий дом дерево  
+// console.log(heading)
+// // console.log(document.getElementById('Hello'))
+
+// // console.dir(heading);//console.dir - показывает свойства объекта
+// //textContent - выводит текст 
+// // heading.textContent = 'Changed from JS'; //замена текста html h1 index hello
+// heading.backgroundColor = 'red'
+// const a = 'string'
+// console.log(a)
+
+
+
+
+
+
+/*
+*/
+
+
+// var array = [2, 5, 9, 15, 0, 4];
+// console.log(array.join())
+// var array1 = [];
+
+// for (let index = 0; index < array.length; index++) {
+//     if (array[index]>3 || array[index]<10) {
+//         // document.write.join(', ', (array[index]));
+//         array1[index] = array[index]; 
+//     }    
+// }
+// console.log(array1.join())
+
+
+// var count = prompt('Счетчик',100);
+// for(let i = count; i > 0; i--)
+// {
+//     let time = setInterval(() => {
+//         console.log(i)
+//     }, interval);
+    
+// }
+
+
